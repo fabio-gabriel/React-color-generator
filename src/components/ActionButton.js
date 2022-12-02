de5505fbbbfbe500e4ba49c8c59602fa79780d97
fabio-gabriel <fabiogabrieleig@gmail.com>
2022-12-01 21:56:59 -0300

    finished 1st version

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -5,9 +5,11 @@ function ActionButton({generateHexColor}) {
   return (
     <div className='ActionButton'>
       <h3>Click on the button to generate a new random color!</h3>
-      <button onClick={generateHexColor}>
-        Heloo
-      </button>
+      <div className='centerButton'>
+        <button onClick={generateHexColor}>
+          Generate
+        </button>
+      </div>
     </div>
   )
 }
